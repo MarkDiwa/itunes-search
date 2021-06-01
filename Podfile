7a40eb2bd852bf1c48e45352e3a6a66d89aa4e80
Mark Dennis Diwa <mark.diwa@cheetahdigital.com>
2021-06-01 20:34:28 +0800

    Add initial project

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iox-exam-cybilltek' do
+  use_frameworks!
+
+  pod 'Alamofire'
+  pod 'Kingfisher'
+  pod 'SwiftLint', '0.43.0'
+  
+
+end
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  itunes-search
+//
+//  Created by Mark Dennis Diwa on 6/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
